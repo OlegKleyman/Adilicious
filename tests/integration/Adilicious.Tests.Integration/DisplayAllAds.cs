03f360dce8c73743f7d598a46b31680c630b925c
Oleg Kleyman <Omego2K@gmail.com>
2015-11-19 19:18:11 -0500

    add step definitions for displaying all ads

---
+++ tests/integration/Adilicious.Tests.Integration/DisplayAllAds.cs
@@ -0,0 +1,44 @@
+﻿namespace Adilicious.Tests.Integration
+{
+    using TechTalk.SpecFlow;
+
+    [Binding]
+    public class DisplayAllAds
+    {
+        [Given(@"I want to display All ads")]
+        public void GivenIWantToDisplayAllAds()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"I should see (.*) ads a page")]
+        public void ThenIShouldSeeAdsAPage(int adCount)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"there should be (.*) pages")]
+        public void ThenThereShouldBePages(int pageCount)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the data should be sorted")]
+        public void ThenTheDataShouldBeSorted()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I click the (.+) column")]
+        public void WhenIClickTheColumn(string columnName)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the data should be sorted")]
+        public void ThenTheDataShouldBeSorted(Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
