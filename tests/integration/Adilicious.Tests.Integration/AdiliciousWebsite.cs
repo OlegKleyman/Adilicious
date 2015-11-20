08b040c1a835fb159f3bdaa2a40c54de55fffb9f
Oleg Kleyman <Omego2K@gmail.com>
2015-11-19 19:42:16 -0500

    ad website start and stop capability

---
+++ tests/integration/Adilicious.Tests.Integration/AdiliciousWebsite.cs
@@ -0,0 +1,32 @@
+﻿namespace Adilicious.Tests.Integration
+{
+    using System.IO;
+
+    using IISExpressAutomation;
+
+    public class AdiliciousWebsite
+    {
+        private IISExpress iis;
+
+        public void Start()
+        {
+            if (iis != null)
+            {
+                throw new InvalidDataException("IIS Express is already running");
+            }
+
+            iis = new IISExpress(new Parameters
+            {
+                Path = Path.GetFullPath(@"..\..\..\..\..\src\Adilicious.Web"),
+                Port = 1337
+            });
+
+        }
+
+        public void Stop()
+        {
+            iis.Dispose();
+            iis = null;
+        }
+    }
+}
---
+++ tests/integration/Adilicious.Tests.Integration/CommonSteps.cs
@@ -0,0 +1,22 @@
+﻿namespace Adilicious.Tests.Integration
+{
+    using TechTalk.SpecFlow;
+
+    [Binding]
+    public class CommonSteps
+    {
+        [BeforeFeature]
+        public static void Setup()
+        {
+            var site = new AdiliciousWebsite();
+            site.Start();
+            FeatureContext.Current.Set(site);
+        }
+
+        [AfterFeature]
+        public static void Teardown()
+        {
+            FeatureContext.Current.Get<AdiliciousWebsite>().Stop();
+        }
+    }
+}
