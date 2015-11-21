dbf6c33a696fc903b18710538966398001a41ae4
Oleg Kleyman <Omego2K@gmail.com>
2015-11-21 17:51:12 -0500

    implement IEquatable in Ad

--- src/Adilicious.Core/Ad.cs
+++ src/Adilicious.Core/Ad.cs
@@ -1,6 +1,8 @@
 namespace Adilicious.Core
 {
-    public class Ad
+    using System;
+
+    public class Ad : IEquatable<Ad>
     {
         public int AdId { get; set; }
 
@@ -9,5 +11,21 @@ namespace Adilicious.Core
         public decimal NumPages { get; set; }
 
         public Position Position { get; set; }
+
+        public bool Equals(Ad other)
+        {
+            if (other == null)
+            {
+                throw new ArgumentNullException("other");
+            }
+
+            return NumPages == other.NumPages;
+        }
+
+        public override int GetHashCode()
+        {
+            var brandHash = Brand == null ? 0 : Brand.GetHashCode();
+            return AdId.GetHashCode() ^ brandHash ^ NumPages.GetHashCode() ^ Position.GetHashCode();
+        }
     }
 }
---
+++ tests/unit/Adilicious.Core.Tests.Unit/AdTests.cs
@@ -0,0 +1,45 @@
+﻿namespace Adilicious.Core.Tests.Unit
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class AdTests
+    {
+        public static object[] equalsShouldEquateAdsSource =
+            {
+                new object[]
+                    {
+                        new Ad { NumPages = 3 },
+                        new Ad { NumPages = 1 },
+                        false
+                    },
+                
+                new object[]
+                    {
+                        new Ad { NumPages = 3 },
+                        new Ad { NumPages = 3 },
+                        true
+                    },
+                
+                new object[]
+                    {
+                        new Ad { NumPages = 1 },
+                        new Ad { NumPages = 3 },
+                        false
+                    }
+            };
+
+        [TestCaseSource("equalsShouldEquateAdsSource")]
+        public void EqualsShouldEquateAds(Ad ad, Ad other, bool expected)
+        {
+            Assert.That(ad.Equals(other), Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void GetHashCodeShouldReturnHash()
+        {
+            var ad = new Ad { Brand = new Brand(10, "test"), NumPages = 3, AdId = 1 };
+            Assert.That(ad.GetHashCode(), Is.EqualTo(-1944423539));
+        }
+    }
+}
