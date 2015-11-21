5c231cade1035f0f63f148554d753880175df7db
Oleg Kleyman <Omego2K@gmail.com>
2015-11-21 14:48:25 -0500

    implement check for all ads being Cover

---
+++ tests/integration/Adilicious.Tests.Integration/CoverSteps.cs
@@ -0,0 +1,20 @@
+﻿namespace Adilicious.Tests.Integration
+{
+    using Adilicious.Core;
+
+    using NUnit.Framework;
+
+    using TechTalk.SpecFlow;
+
+    [Binding]
+    public class CoverSteps
+    {
+        [Then(@"all results should have a cover position")]
+        public void ThenAllResultsShouldHaveACoverPosition()
+        {
+            var allAds = ScenarioContext.Current.Get<AdiliciousPage>().GetAllAvailableAds();
+
+            Assert.That(allAds, Has.All.Property("Position").EqualTo(Position.Cover));
+        }
+    }
+}
