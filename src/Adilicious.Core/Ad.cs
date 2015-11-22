dc9398af8ab438c220709ce5c9385c0b311ea0f5
Oleg Kleyman <Omego2K@gmail.com>
2015-11-21 19:19:20 -0500

    add FromData method to Ad class

--- src/Adilicious.Core/Ad.cs
+++ src/Adilicious.Core/Ad.cs
@@ -1,6 +1,7 @@
 namespace Adilicious.Core
 {
     using System;
+    using System.Globalization;
 
     public class Ad
     {
@@ -11,5 +12,32 @@ namespace Adilicious.Core
         public decimal NumPages { get; set; }
 
         public Position Position { get; set; }
+
+        public static Ad FromDataAd(Mediaradar.Ad dataAd)
+        {
+            const string adParamName = "ad";
+
+            if (dataAd == null)
+            {
+                throw new ArgumentNullException(adParamName);
+            }
+
+            Position position;
+
+            if (!Enum.TryParse(dataAd.Position, true, out position))
+            {
+                throw new ArgumentException(
+                    String.Format(CultureInfo.InvariantCulture, "Invalid position: {0}", dataAd.Position),
+                    adParamName);
+            }
+
+            return new Ad
+                       {
+                           Brand = Brand.FromDataBrand(dataAd.Brand),
+                           Position = position,
+                           NumPages = dataAd.NumPages,
+                           AdId = dataAd.AdId
+                       };
+        }
     }
 }
---
+++ tests/unit/Adilicious.Core.Tests.Unit/AdTests.cs
@@ -0,0 +1,32 @@
+﻿namespace Adilicious.Core.Tests.Unit
+{
+    using Adilicious.Core.Mediaradar;
+
+    using NUnit.Framework;
+
+    using Ad = Adilicious.Core.Ad;
+
+    [TestFixture]
+    public class AdTests
+    {
+        [Test]
+        public void FromDataAdShouldReturnBusinessAd()
+        {
+            var ad =
+                Ad.FromDataAd(
+                    new Mediaradar.Ad
+                        {
+                            Brand = new Brand { BrandName = "Test", BrandId = 1 },
+                            Position = "Cover",
+                            NumPages = 1,
+                            AdId = 2
+                        });
+
+            Assert.That(ad.Brand.Name, Is.EqualTo("Test"));
+            Assert.That(ad.Brand.Id, Is.EqualTo(1));
+            Assert.That(ad.Position, Is.EqualTo(Position.Cover));
+            Assert.That(ad.NumPages, Is.EqualTo(1));
+            Assert.That(ad.AdId, Is.EqualTo(2));
+        }
+    }
+}
